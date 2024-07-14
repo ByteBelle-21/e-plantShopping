4dae090fd166f106a4754f2ebc4bf2fad9eb1554
ByteBelle-21 <jrg814@usask.ca>
2024-07-14 18:00:44 -0400

    Displayed the total number of items in the cart on the cart logo

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
-import {useDispatch } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { addItem } from './CreatSlice';
 function ProductList() {
     const dispatch = useDispatch()
@@ -237,6 +237,11 @@ function ProductList() {
       dispatch(addItem(product));
       setAddedToCart((prev)=>({...prev,[product.name]:true}))
    }
+   const cart = useSelector(state => state.cart.items);
+   const[ItemCount, setItemCount] = useState(cart.length);
+   useEffect(()=>{
+    setItemCount(cart.length);
+   },[cart]);
    
 
 
@@ -257,7 +262,9 @@ function ProductList() {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" style={styleA}>Plants</a></div>
-                <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                <text x="50%" y="60%" dominantBaseline="middle" textAnchor="middle" fill="#000000" fontSize="70">{ItemCount}</text>
+                </svg></h1></a></div>
             </div>
         </div>
        
