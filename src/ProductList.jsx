fbbb9639ee2ae3acab9fc4ae5689c9d7a044cc78
ByteBelle-21 <jrg814@usask.ca>
2024-07-14 15:47:50 -0400

    Added product list to the web page

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -249,9 +249,24 @@ function ProductList() {
                 <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
         </div>
-
+       
         <div className="product-grid">
-
+            {plantsArray.map((Category)=>(
+                <div>
+                    <div className='product-category'>{Category.category}</div>
+                    <div className='product-list'>
+                        {Category.plants.map((plant)=>(
+                            <div className='product-card'>
+                                <img className='product-image' src={plant.image} alt={plant.name}/>
+                                <div className='product-title'>{plant.name}</div>
+                                <p>{plant.description}</p>
+                                <div className='product-cost'>{plant.cost}</div>
+                                <button className='product-button'>Add To Cart</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
 
         </div>
 
